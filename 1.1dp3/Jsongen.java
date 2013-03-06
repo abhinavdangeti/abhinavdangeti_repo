1a28dd5ae667075e390e723a9f5689ee5f69be11
abhinavdangeti <abhinav@couchbase.com>
2013-03-05 16:22:06 -0800

    Padding

--- 1.1dp3/Jsongen.java
+++ 1.1dp3/Jsongen.java
@@ -16,22 +16,22 @@ public class Jsongen {
         }
         if(temp % 2 == 0){
             _number = "e" + temp.toString();
-            jsonobj = new JSONObject("{\"planet\":\"earth\", \"species\":\"human\", \"number\":\"" + _number + "\", \" padding \":\"" + _padding + "\"}");
+            jsonobj = new JSONObject("{\"planet\":\"earth\", \"species\":\"human\", \"number\":\"" + _number + "\", \"padding\":\"" + _padding + "\"}");
         } else if(temp % 3 == 0) {
             _number = "m" + temp.toString();
-            jsonobj = new JSONObject("{\"planet\":\"mars\", \"species\":\"martian\", \"number\":\"" + _number + "\", \" padding \":\"" + _padding + "\"}");
+            jsonobj = new JSONObject("{\"planet\":\"mars\", \"species\":\"martian\", \"number\":\"" + _number + "\", \"padding\":\"" + _padding + "\"}");
         } else if(temp % 10 == 1) {
             _number = "v" + temp.toString();
-            jsonobj = new JSONObject("{\"planet\":\"venus\", \"species\":\"venusses\", \"number\":\"" + _number + "\", \" padding \":\"" + _padding + "\"}");
+            jsonobj = new JSONObject("{\"planet\":\"venus\", \"species\":\"venusses\", \"number\":\"" + _number + "\", \"padding\":\"" + _padding + "\"}");
         } else if(temp % 10 == 3) {
             _number = "j" + temp.toString();
-            jsonobj = new JSONObject("{\"planet\":\"jupiter\", \"species\":\"jupitorian\", \"number\":\"" + _number + "\", \" padding \":\"" + _padding + "\"}");
+            jsonobj = new JSONObject("{\"planet\":\"jupiter\", \"species\":\"jupitorian\", \"number\":\"" + _number + "\", \"padding\":\"" + _padding + "\"}");
         } else if(temp % 10 == 5) {
             _number = "s" + temp.toString();
-            jsonobj = new JSONObject("{\"planet\":\"saturn\", \"species\":\"saturness\", \"number\":\"" + _number + "\", \" padding \":\"" + _padding + "\"}");
+            jsonobj = new JSONObject("{\"planet\":\"saturn\", \"species\":\"saturness\", \"number\":\"" + _number + "\", \"padding\":\"" + _padding + "\"}");
 		} else {
             _number = "u" + temp.toString();
-            jsonobj = new JSONObject("{\"planet\":\"unknown\", \"species\":\"unknown\", \"number\":\"" + _number + "\", \" padding \":\"" + _padding + "\"}");
+            jsonobj = new JSONObject("{\"planet\":\"unknown\", \"species\":\"unknown\", \"number\":\"" + _number + "\", \"padding\":\"" + _padding + "\"}");
         }
 		return jsonobj;
 	}
