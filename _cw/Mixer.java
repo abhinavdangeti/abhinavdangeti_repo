fef1ef1e8181fd41af6e90b8ad9b72ab0c0a06d5
abhinav dangeti <abhinav@couchbase.com>
2013-03-19 12:18:45 -0700

    Update Mixer.java

--- _cw/Mixer.java
+++ _cw/Mixer.java
@@ -167,7 +167,7 @@ public class Mixer {
 			
 			expire_post(0, _initial_load, _expiration_time, dclient);
 			
-			System.out.println("\n --< WAIT FOR ABOUT HALF AN HOUR BEFORE MANUALLY RUNNING EXPIRY PAGER ON DEFAULT and then hit ENTER (RUN " + (k++) + ") >-- \n");
+			System.out.println("\n --< WAIT FOR ABOUT HALF AN HOUR BEFORE MANUALLY RUNNING EXPIRY PAGER ON DEFAULT and then hit ENTER (RUN " + k + ") >-- \n");
 			sc = new Scanner(System.in);
 			@SuppressWarnings("unused")
 			String _ok_3 = sc.nextLine();
