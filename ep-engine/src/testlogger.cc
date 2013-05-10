e5a4c90926ea02a4822e043b319593c4efd26fb8
abhinavdangeti <abhinav@couchbase.com>
2013-05-10 16:04:47 -0700

    Removing unused files

--- ep-engine/src/testlogger.cc
+++
@@ -1,9 +0,0 @@
-/* -*- Mode: C; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
-
-#include "config.h"
-
-#include "common.hh"
-
-void LOG(EXTENSION_LOG_LEVEL severity, const char *fmt, ...) {
-    //ignore
-}
