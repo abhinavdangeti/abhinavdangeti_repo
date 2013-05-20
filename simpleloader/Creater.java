51f4320447c60a1d8b9d9350912841bc356c411d
abhinavdangeti <abhinav@couchbase.com>
2013-05-20 13:42:31 -0700

    Updating

--- simpleloader/Creater.java
+++ simpleloader/Creater.java
@@ -15,33 +15,33 @@ import com.couchbase.client.CouchbaseClient;
 
 public class Creater {
 
-	public static void create_items (int _itemCount, int _itemSize, boolean _json, CouchbaseClient client, String _prefix) 
-			throws URISyntaxException, IOException, JSONException, InterruptedException, ExecutionException {
-		Random gen = new Random ( 987654321 );
-		StringBuffer value = new StringBuffer();
-		String CHAR_LIST = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        while (value.length() < _itemSize) {
-           value.append(CHAR_LIST);
-        }
-        
-        List<OperationFuture<Boolean>> creates = new LinkedList<OperationFuture<Boolean>>();
-		for (int i=0; i<_itemCount; i++){
-			OperationFuture<Boolean> setOp;
-			String key = String.format("%s%d", _prefix, i);
-			if (_json) {
-				JSONObject _val = Spawner.retrieveJSON(gen, _itemSize);
-				setOp = client.set(key, 0, _val.toString());
-			} else {
-				setOp = client.set(key, 0, value.toString());
-				creates.add(setOp);
-			}
+    public static void create_items (int _itemCount, int _itemSize, boolean _json, CouchbaseClient client, String _prefix) 
+	throws URISyntaxException, IOException, JSONException, InterruptedException, ExecutionException {
+	    Random gen = new Random ( 987654321 );
+	    StringBuffer value = new StringBuffer();
+	    String CHAR_LIST = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	    while (value.length() < _itemSize) {
+		value.append(CHAR_LIST);
+	    }
+
+	    List<OperationFuture<Boolean>> creates = new LinkedList<OperationFuture<Boolean>>();
+	    for (int i=0; i<_itemCount; i++){
+		OperationFuture<Boolean> setOp;
+		String key = String.format("%s%d", _prefix, i);
+		if (_json) {
+		    JSONObject _val = Spawner.retrieveJSON(gen, _itemSize);
+		    setOp = client.set(key, 0, _val.toString());
+		} else {
+		    setOp = client.set(key, 0, value.toString());
+		    creates.add(setOp);
 		}
-		while (!creates.isEmpty()) {
-			if (creates.get(0).get().booleanValue() == false){
-				System.err.println("Set failed: "/* + setOp.getStatus().getMessage()*/);
-				continue;
-			}
-			creates.remove(0);
+	    }
+	    while (!creates.isEmpty()) {
+		if (creates.get(0).get().booleanValue() == false){
+		    System.err.println("Set failed: "/* + setOp.getStatus().getMessage()*/);
+		    continue;
 		}
+		creates.remove(0);
+	    }
 	}
 }
--- simpleloader/Deleter.java
+++ simpleloader/Deleter.java
@@ -15,12 +15,12 @@ import com.couchbase.client.CouchbaseClient;
 
 public class Deleter {
 
-	public static void del_items (int _itemCount, CouchbaseClient client, String _prefix) 
-			throws URISyntaxException, IOException, JSONException, InterruptedException, ExecutionException {
-		for (int i=0; i<_itemCount; i++) {
-			OperationFuture<Boolean> delOp;
-			String key = String.format("%s%d", _prefix, i);
-			delOp = client.delete(key);
-		}
+    public static void del_items (int _itemCount, CouchbaseClient client, String _prefix) 
+	throws URISyntaxException, IOException, JSONException, InterruptedException, ExecutionException {
+	    for (int i=0; i<_itemCount; i++) {
+		OperationFuture<Boolean> delOp;
+		String key = String.format("%s%d", _prefix, i);
+		delOp = client.delete(key);
+	    }
 	}
 }
