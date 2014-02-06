91d8a4796cff9e5bb475cdefab3d73954c20e769
abhinavdangeti <abhinav@couchbase.com>
2014-02-06 15:49:56 -0800

    Remove unused tools/files

--- pysystests/webapp/cbmonitor/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-
-urlpatterns = patterns(
-    '',
-    url(r'^$', 'cbmonitor.views.index'),
-    url(r'/(?P<path>[a-z_]+)/$', 'cbmonitor.rest_api.dispatcher'),
-)
